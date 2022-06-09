537ac38e5c0b9c6074a788f4dd3f8fe176b0e3e5
Eradev <admin@eradev.com>
2022-06-08 21:53:24 -0400

    Fix pickup verification

--- ShareSuite/ItemSharingHooks.cs
+++ ShareSuite/ItemSharingHooks.cs
@@ -476,7 +476,7 @@ namespace ShareSuite
                     case ItemTier.VoidTier2:
                     case ItemTier.VoidTier3:
                     case ItemTier.VoidBoss:
-                        return ShareSuite.VoidItemsShared.Value || ShareSuite.VoidItemsShared.Value;
+                        return ShareSuite.VoidItemsShared.Value || ShareSuite.ShareVoidItemsAsBase.Value;
                     case ItemTier.NoTier:
                         break;
                     case ItemTier.AssignedAtRuntime:
